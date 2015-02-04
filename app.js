7e64073056f040d6939da35f45b87832a0e9474a
hqythu <hqythu@gmail.com>
2015-02-04 14:58:44 +0800

    change to swig template engine

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +12,11 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+swig.setDefaults({ loader: swig.loaders.fs(path.join(__dirname, 'views')) });
+//swig.setDefaults({ cache: nconf.get('debug') ? false: 'memory' });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
