82f5406b9ba7d1fc690e3b31f2d52b087835da74
hqythu <hqythu@gmail.com>
2015-05-09 19:14:30 +0800

    add command handler

--- app/main/handler.py
+++ app/main/handler.py
@@ -6,24 +6,41 @@ from ..models import Task
 from datetime import datetime
 
 
+def view_tasks(user):
+    return '还没有开发出来哦'
+
+
+def query_credits(user):
+    return '您的现有积分为: %s' % user.credits
+
+
+commands = {
+    '任务': view_tasks,
+    '积分': query_credits,
+    '查询': query_credits
+}
+
+
 def handler(message):
     openid = message.get('FromUserName', '')
     user = User.query.filter_by(openid=openid).first()
     if user is None:
         raise UserNotRegisteredException()
-    try:
+    if message['Content'] in commands:
+        reply = commands[message['Content']](user)
+    elif message['Content'] in TaskList:
         task_config = TaskList[message['Content']]
-    except KeyError:
+        task_config['validator'](user)
+        user.credits += task_config['credit']
+        task = Task(name=task_config['name'], credit=task_config['credit'],
+                    datetime=datetime.now(), user=user)
+        db.session.add(user)
+        db.session.add(task)
+        db.session.commit()
+        reply = "您成功完成了任务：%s，获得了积分：%d" % (task.name, task.credit)
+    else:
         raise CommandNotFoundException()
-    task_config['validator'](user)
-    user.credits += task_config['credit']
-    task = Task(name=task_config['name'], credit=task_config['credit'],
-                datetime=datetime.now(), user=user)
-    db.session.add(user)
-    db.session.add(task)
-    db.session.commit()
-    return construct_reply_message(message,
-                                   "您成功完成了任务：%s，获得了积分：%d" % (task.name, task.credit))
+    return construct_reply_message(message, reply)
 
 
 def construct_reply_message(msg, text):
