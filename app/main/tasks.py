c159e9303e615645315d5abbee0c8ea24ea46e69
hqythu <hqythu@gmail.com>
2015-05-10 00:56:40 +0800

    use high-order function to generate validator

--- app/main/tasks.py
+++ app/main/tasks.py
@@ -11,34 +11,37 @@ def empty_validator(user):
     pass
 
 
-def eachday_validator(user):
-    recent_task = user.tasks.filter_by(key='1').order_by(Task.datetime.desc()).first()
-    if recent_task is None:
-        return
-    now = (datetime.utcnow() + timedelta(hours=8)).date()
-    if (recent_task.datetime + timedelta(hours=8)).date() == now:
-        raise AlreadyDoTodayException()
+def eachday_validator_generator(task_key):
+    def eachday_validator(user):
+        recent_task = user.tasks.filter_by(key=task_key).order_by(Task.datetime.desc()).first()
+        if recent_task is None:
+           return
+        now = (datetime.utcnow() + timedelta(hours=8)).date()
+        if (recent_task.datetime + timedelta(hours=8)).date() == now:
+            raise AlreadyDoTodayException()
+    return eachday_validator
 
 
-def time_validator(user):
-    now = (datetime.utcnow() + timedelta(hours=8)).time()
-    start = time(hour=6, minute=0)
-    end = time(hour=7, minute=0)
-    if start <= now <= end:
-        pass
-    else:
-        raise TimeNotMatchException()
+def time_validator_generator(start, end):
+    def time_validator(user):
+        now = (datetime.utcnow() + timedelta(hours=8)).time()
+        if start <= now <= end:
+            pass
+        else:
+            raise TimeNotMatchException()
+    return time_validator
 
 
 TaskList = {
     '1': {
         'name': '1',
-        'validator': eachday_validator,
+        'validator': eachday_validator_generator('1'),
         'credit': 1
     },
     '2': {
         'name': '2',
-        'validator': time_validator,
+        'validator': time_validator_generator(time(hour=6, minute=0),
+                                              time(hour=7, minute=0)),
         'credit': 2
     },
     '3': {
