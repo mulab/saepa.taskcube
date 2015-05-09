57c43ae5d1fd6fe3e2f471b9935a7f6e120b9972
hqythu <hqythu@gmail.com>
2015-05-09 15:48:08 +0800

    添加任务的Model

--- app/models.py
+++ app/models.py
@@ -9,6 +9,20 @@ class User(db.Model):
     mobile = db.Column(db.Unicode(64), default='')
     openid = db.Column(db.Unicode(128), default='', unique=True, index=True)
     credits = db.Column(db.Integer, default=0)
+    tasks = db.relationship('Task', backref='user')
 
     def __repr__(self):
         return '<User: %s, ID: %s, Credits: %d>' % (self.name, self.openid, self.credits)
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(64))
+    credits = db.Column(db.Integer)
+    datetime = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.openid'))
+
+    def __repr__(self):
+        return '<Task: %s, Credits: %s, User: %d, DateTime: %s>' % \
+               (self.name, self.credits, self.user_id, str(self.datetime))
